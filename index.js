8b1fe1dde35a28bfc3fcd081ab228395750c8130
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2021-09-27 16:09:11 -0700

    Send a test board

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const app = express();
+const {createBoardGenerator} = require('./createBoardGenerator');
 // eslint-disable-next-line new-cap
-const router = express.Router();
+// const router = express.Router();
 // const cors = require("cors");
 
 app.use(express.json());
 
+const boardGen = createBoardGenerator();
+const testBoard = boardGen.generateBoardWithCheckmate(
+    {
+      'w_p': 2,
+      'b_p': 2,
+      'w_q': 1,
+      'w_r': 1,
+      'b_r': 1,
+      'w_k': 1,
+      'b_k': 1,
+    });
+
 app.get('/', (req, res) => {
-  res.send('<h1>hello world</h1>');
+  if (!testBoard) res.send('<div>Still generating board</div>');
+  res.send({brd: testBoard});
 });
 
 const port = process.env.PORT || 5000;
--- testing.js
+++ testing.js
@@ -2,20 +2,20 @@ const {createBoard} = require('./createBoard');
 const {createSolver} = require('./createSolver');
 const {createBoardGenerator} = require('./createBoardGenerator');
 
-// const boardGen = createBoardGenerator();
-// const testBoard = boardGen.generateBoardWithCheckmate(
-//     {
-//       'w_p': 3,
-//       'b_p': 3,
-//       'w_q': 1,
-//       'w_r': 1,
-//       'b_r': 2,
-//       'w_k': 1,
-//       'b_k': 1,
-//     });
+const boardGen = createBoardGenerator();
+const testBoard = boardGen.generateBoardWithCheckmate(
+    {
+      'w_p': 2,
+      'b_p': 2,
+      'w_q': 1,
+      'w_r': 1,
+      'b_r': 1,
+      'w_k': 1,
+      'b_k': 1,
+    });
 
 // =====
-const testBoard = createBoard();
+// const testBoard = createBoard();
 
 // test for easy checkmate
 // testBoard.addPiece(7, 7, 'b', 'k');
@@ -34,19 +34,19 @@ const testBoard = createBoard();
 // testBoard.addPiece(0, 7, 'w', 'p');
 
 // random testing
-testBoard.addPiece(0, 2, 'b', 'r');
-testBoard.addPiece(0, 7, 'b', 'k');
-testBoard.addPiece(1, 0, 'b', 'r');
-testBoard.addPiece(2, 6, 'b', 'p');
-testBoard.addPiece(3, 1, 'b', 'p');
-testBoard.addPiece(3, 7, 'b', 'p');
+// testBoard.addPiece(0, 2, 'b', 'r');
+// testBoard.addPiece(0, 7, 'b', 'k');
+// testBoard.addPiece(1, 0, 'b', 'r');
+// testBoard.addPiece(2, 6, 'b', 'p');
+// testBoard.addPiece(3, 1, 'b', 'p');
+// testBoard.addPiece(3, 7, 'b', 'p');
 
-testBoard.addPiece(2, 7, 'w', 'k');
-testBoard.addPiece(3, 0, 'w', 'r');
-testBoard.addPiece(4, 5, 'w', 'q');
-testBoard.addPiece(5, 0, 'w', 'p');
-testBoard.addPiece(5, 3, 'w', 'p');
-testBoard.addPiece(5, 5, 'w', 'p');
+// testBoard.addPiece(2, 7, 'w', 'k');
+// testBoard.addPiece(3, 0, 'w', 'r');
+// testBoard.addPiece(4, 5, 'w', 'q');
+// testBoard.addPiece(5, 0, 'w', 'p');
+// testBoard.addPiece(5, 3, 'w', 'p');
+// testBoard.addPiece(5, 5, 'w', 'p');
 const testSolver = createSolver(testBoard);
 testSolver.buildMoveTree(testBoard, 'w', 0, 3);
 testSolver.buildBestMoves(testBoard, 'w');
